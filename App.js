fce3b0a2abcd2960f48d34e824f46340370aee9f
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2023-05-17 10:16:10 -0300

    ✨ feat(app): onesignal setup

--- App.js
+++ App.js
@@ -8,13 +8,69 @@ import { useState, useEffect } from "react";
 import AuthContext from "./src/contexts/auth";
 import PopupContext from "./src/contexts/popup";
 import Routes from "./src/routes/index";
+import OneSignal from "react-native-onesignal";
+import Constants from 'expo-constants';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+OneSignal.setAppId(Constants.manifest.extra.oneSignalAppId);
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
   const [user, setUser] = useState(null);
   const [popup, setPopup] = useState(false);
+  const [expoPushToken, setExpoPushToken] = useState('');
 
+  async function registerForPushNotificationsAsync() {
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  }
 
-  // async function requestUserPermission() {
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+  }, [])
+
+  const [loaded] = useFonts({
+    Comfortaa_400Regular,
+    Comfortaa_700Bold,
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <AuthContext.Provider value={{ signed: !!user, user, setUser }}>
+        <PopupContext.Provider value={{ popup, setPopup }}>
+          <Routes />
+        </PopupContext.Provider>
+      </AuthContext.Provider>
+    </NavigationContainer>
+  );
+}
+
+// async function requestUserPermission() {
   //   const authStatus = await messaging().requestPermission();
   //   const enabled =
   //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
@@ -65,23 +121,3 @@ export default function App() {
 
   //   return unsubscribe;
   // }, [])
-
-  const [loaded] = useFonts({
-    Comfortaa_400Regular,
-    Comfortaa_700Bold,
-  });
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <NavigationContainer>
-      <AuthContext.Provider value={{ signed: !!user, user, setUser }}>
-        <PopupContext.Provider value={{ popup, setPopup }}>
-          <Routes />
-        </PopupContext.Provider>
-      </AuthContext.Provider>
-    </NavigationContainer>
-  );
-}
