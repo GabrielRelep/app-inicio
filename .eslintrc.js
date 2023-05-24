6a20ef4c9521826046f4103735b7d8615c37140e
ryanalencar-dev <ryanalencar@febracis.com.br>
2023-05-24 10:08:49 -0300

    ✨ feat(app): setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'universe'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
